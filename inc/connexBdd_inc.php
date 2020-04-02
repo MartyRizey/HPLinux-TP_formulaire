31b31d8f60b0195c5c55a9053764e14379dc3c14
MartyRizey <martyrizey@gmail.com>
2020-04-02 14:43:41 +0200

    Ajout fichier 'configBdd_inc.php' et 'insertDatasReg_mod.php'

--- inc/connexBdd_inc.php
+++ inc/connexBdd_inc.php
@@ -1,6 +1,6 @@
 <?php
   
-  require_once ROOTPATH . '/inc/inc_configBdd.php';
+  require_once ROOTPATH . '/inc/configBdd_inc.php';
 
   // Connexion => https://www.php.net/manual/fr/pdo.construct.php
   // PDO::__construct — Crée une instance PDO qui représente une connexion à la base
