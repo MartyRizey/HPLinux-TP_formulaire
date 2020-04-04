e63e2775ada38220abc2eb9a68d0a95480adb6d5
MartyRizey <martyrizey@gmail.com>
2020-04-04 11:06:11 +0200

    Ajout de message si une session existe - modif dans home_vws.php

--- views/home_vws.php
+++ views/home_vws.php
@@ -13,20 +13,23 @@
         <p class="home_page-date"><?= date('l d F Y'); ?></p>
 
         <?php
-/*
+
             // message selon si la session existe ou pas
             if(isset($_SESSION)) {
 
                 // Condition ternaire => https://www.php.net/manual/fr/language.operators.comparison.php
                 // Verifie si une clé existe dans le tableau => https://www.php.net/manual/fr/function.array-key-exists.php
-                echo $message = (!array_key_exixts('pseudo', $_SESSION))
+                echo $message = (!array_key_exists('pseudo', $_SESSION))
 
                     // en fonction de si une personne est connéctée on affichera un des 2 messages qui sera stocké dans $message.
                     ? '<h3 class="home_page-message">Pensez à vous inscrire si vous êtes un nouvel utilisateur.</h3>' 
                     : 'Bienvenu(e) dans votre session : ' . $_SESSION['pseudo'];
 
+            } else {
+
+                echo 'echec';
             }
-*/
+
         ?>    
     
         <h1 class="home_page-title">Page Home</h1>
